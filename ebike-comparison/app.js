cacd2a5500f3149eb78dfa0710a0cefa429156f0
Sean Aguinaga <saguinag@gmail.com>
2026-07-12 15:03:14 -0700

    Deploy: spec row and efficiency padding fix

--- ebike-comparison/app.js
+++ ebike-comparison/app.js
@@ -1,2 +1,2 @@
 /* Loader for cached index.html still requesting app.js */
-(function(){var s=document.createElement("script");s.src="app.202607122200.js";s.defer=true;document.head.appendChild(s);})();
+(function(){var s=document.createElement("script");s.src="app.202607122203.js";s.defer=true;document.head.appendChild(s);})();
