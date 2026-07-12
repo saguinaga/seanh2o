168e3cf3ffc3c4cd9e406da45f809948cdf45f32
Sean Aguinaga <saguinag@gmail.com>
2026-07-12 15:22:40 -0700

    Rename chat agent to Ryan with thinking indicator

--- ebike-comparison/app.js
+++ ebike-comparison/app.js
@@ -1,2 +1,2 @@
 /* Loader for cached index.html still requesting app.js */
-(function(){var s=document.createElement("script");s.src="app.202607122221.js?v=202607122221";s.defer=true;document.head.appendChild(s);})();
+(function(){var s=document.createElement("script");s.src="app.202607122222.js?v=202607122222";s.defer=true;document.head.appendChild(s);})();
