8fd6b06db55e61252c9bff5bc0902325ab600794
Sean Aguinaga <saguinag@gmail.com>
2026-07-12 17:19:46 -0700

    Deploy configurable rider age for legal checks

--- ebike-comparison/app.js
+++ ebike-comparison/app.js
@@ -1,2 +1,2 @@
 /* Loader for cached index.html still requesting app.js */
-(function(){var s=document.createElement("script");s.src="app.202607130008.js?v=202607130008";s.defer=true;document.head.appendChild(s);})();
+(function(){var s=document.createElement("script");s.src="app.202607130019.js?v=202607130019";s.defer=true;document.head.appendChild(s);})();
